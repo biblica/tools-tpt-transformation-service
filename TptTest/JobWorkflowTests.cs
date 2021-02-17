9b027098842bad1aae140f7ae7ca4098c332d6b5
Christian M. Macy <christian.macy@opsysinc.com>
2021-02-17 09:36:06 -0700

    DT-27: (Minor Refactor) Remove superfluous nullable annotations

--- TptTest/JobWorkflowTests.cs
+++ TptTest/JobWorkflowTests.cs
@@ -226,7 +226,7 @@ namespace TptTest
                     It.Is<FileInfo>(it => it.FullName.Equals(testFileInfo.FullName)), It.IsAny<CancellationToken?>()),
                 Times.Once);
             _mockScriptRunner.Verify(runnerItem =>
-                runnerItem.RunScript(testPreviewJob, It.IsAny<string[]?>(), It.IsAny<string>(), It.IsAny<CancellationToken?>()), Times.Once);
+                runnerItem.RunScript(testPreviewJob, It.IsAny<string[]>(), It.IsAny<string>(), It.IsAny<CancellationToken?>()), Times.Once);
             _mockJobManager.Verify(managerItem =>
                 managerItem.TryUpdateJob(testPreviewJob), Times.AtLeastOnce);
             Assert.IsTrue(isTaskRun); // task was run
@@ -281,7 +281,7 @@ namespace TptTest
                     It.Is<FileInfo>(it => it.FullName.Equals(testFileInfo.FullName)), It.IsAny<CancellationToken?>()))
                 .Verifiable();
             _mockScriptRunner.Setup(runnerItem =>
-                runnerItem.RunScript(testPreviewJob, It.IsAny<string[]?>(), It.IsAny<string>(), It.IsAny<CancellationToken?>()))
+                runnerItem.RunScript(testPreviewJob, It.IsAny<string[]>(), It.IsAny<string>(), It.IsAny<CancellationToken?>()))
                 .Callback<PreviewJob, string[], string, CancellationToken?>((jobItem, _, __, tokenItem) =>
                 {
                     isTaskRun = true;
@@ -322,7 +322,7 @@ namespace TptTest
                     It.Is<FileInfo>(it => it.FullName.Equals(testFileInfo.FullName)), It.IsAny<CancellationToken?>()),
                 Times.Once);
             _mockScriptRunner.Verify(runnerItem =>
-                runnerItem.RunScript(testPreviewJob, It.IsAny<string[]?>(), It.IsAny<string>(), It.IsAny<CancellationToken?>()), Times.Once);
+                runnerItem.RunScript(testPreviewJob, It.IsAny<string[]>(), It.IsAny<string>(), It.IsAny<CancellationToken?>()), Times.Once);
             _mockJobManager.Verify(managerItem =>
                 managerItem.TryUpdateJob(testPreviewJob), Times.AtLeastOnce);
             Assert.IsTrue(mockWorkflow.Object.IsJobCanceled); // job wasn't cancelled
